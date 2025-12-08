7cfbc474a2edf2615f6601c1d5e48fe49696f231
jibin <jibinkrishna0010@gmail.com>
2025-12-08 22:08:50 +0530

    Create contact section

--- personal_website/src/App.jsx
+++ personal_website/src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home.jsx";
 import { NotFound } from "./pages/NotFound.jsx";
+import {Toaster} from "@/components/ui/toaster";
 
 function App() {
   return (
+    <>
+    <Toaster />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />     {/* FIXED */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
