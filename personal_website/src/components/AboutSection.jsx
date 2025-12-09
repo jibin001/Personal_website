10bf25817a54736405492d6e7ac4ba2cc7165743
jibin <jibinkrishna0010@gmail.com>
2025-12-09 10:00:20 +0530

    Create Download CV section

--- personal_website/src/components/AboutSection.jsx
+++ personal_website/src/components/AboutSection.jsx
@@ -21,7 +21,7 @@ export const AboutSection = () => {
                                 Get in Touch
                             </a>
 
-                            <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a href="/jibinResume.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download CV
                             </a>
                         </div>
